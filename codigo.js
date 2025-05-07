466442bce9b9108355df067f611eb35e253e3e4c
Mario Rojas <edwinmachucachavez@gmail.com>
2025-05-06 21:22:25 -0300

    version 2

--- codigo.js
+++ codigo.js
@@ -1,3 +1,22 @@
-var x
-x = 10
-console.log(x)
+
+function mostrarDato(){
+
+var input =  document.getElementById("inputdato");
+var parrafo = document.getElementById("resultado");
+var total = document.getElementById("total");
+ parrafo.innerText =  parrafo.innerText += "el dato ingresado es: " + input.value + "\n";
+ suma += parseInt.input.value;
+ total.innerText = suma;
+}
+
+var suma = 0;
+
+
+
+
+ 
+
+
+
+
+// document.getElementById("botonmostrar").onclick = mostrarDato;
