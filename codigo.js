7dbe0cc988e9bb4898a4134976762cf3a63b3fe7
edwin <edwinmachucachavez@gmail.com>
2025-05-13 20:36:27 -0300

    version3

--- codigo.js
+++ codigo.js
@@ -4,9 +4,16 @@ function mostrarDato(){
 var input =  document.getElementById("inputdato");
 var parrafo = document.getElementById("resultado");
 var total = document.getElementById("total");
- parrafo.innerText =  parrafo.innerText += "el dato ingresado es: " + input.value + "\n";
  suma = input.value;
  total.innerText = suma;
+
+ var c = 1
+
+ while ( c < 13 ){
+    parrafo.innerText =   parrafo.innerText   + input.value+ ' x ' + c + ' = '+ (suma * c) + "\n";
+    c++;
+    
+ }
 }
 
 var suma = 0;
